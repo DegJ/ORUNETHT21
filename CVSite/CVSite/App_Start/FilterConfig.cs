23c5786b629d6e99d16905dd8882d08df0af050b
Nicklas Magnusson <nm@impera.se>
2021-12-07 12:16:12 +0100

    init cvsite

---
+++ CVSite/CVSite/App_Start/FilterConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace Cvsite {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CVSite/CVSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cvsite.MvcApplication" Language="C#" %>
